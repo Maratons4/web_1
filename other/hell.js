672817d263db221460d326462ad5c90caafa71bb
BuildTools <unconfigured@null.spigotmc.org>
2022-04-09 15:48:41 +0300

    first commit

---
+++ other/hell.js
@@ -0,0 +1,9 @@
+function hell() {
+    document.getElementById("bg").style.backgroundColor = "blue";
+    document.getElementById("title").innerHTML = "This will now be after you &#128520";                                         // Changes text on ID
+    document.getElementById("title").style.color = "red";                                                                       // Changes Color on ID
+    document.getElementById("title").style.backgroundColor = "yellow";                                                          // Changes BGColor on ID
+    document.getElementById("id1").style.backgroundColor = "red";
+    document.getElementById("img1").src = "images/infiniteGoldenHeads.jpg";                                                     // Changes image on ID
+    document.getElementById("goldHead").innerHTML = "go back to safety";
+}
